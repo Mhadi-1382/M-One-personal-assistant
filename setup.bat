a3523a2b7e386707956ccd092ed0cc2115f8f0aa
Mahdi <82710784+Mhadi-1382@users.noreply.github.com>
2022-06-11 20:15:22 +0430

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,14 @@
+@REM Installing Packages To Setting Up M-One
+
+@echo off
+title Installing Packages
+
+echo Installing Packages
+
+
+pip install -r packages.txt
+
+
+echo Installing Completed.
+
+pause
